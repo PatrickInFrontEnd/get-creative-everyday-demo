6e54fa823e234ad3bd48460a09efe2ea7d950587
PatrickInFrontend <patryk.pluciennik022@gmail.com>
2024-06-01 13:58:51 +0200

    feat: added a component for info that site is no longer used

---
+++ src/layout/SiteIsNotBeingUsedInfo.js
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+    width: 100%;
+    height: auto;
+    position: fixed;
+    bottom: 0;
+    left: 0;
+    padding: 10px 20px;
+    background-color: ${({ theme }) => theme.colors.black};
+    color: ${({ theme }) => theme.colors.yellow};
+`;
+
+const Text = styled.p`
+    font-size: ${({ theme }) => theme.fz.S};
+    font-weight: ${({ theme }) => theme.fw.bold};
+    text-align: center;
+`;
+
+const SiteIsNotBeingUsedInfo = ({ children }) => (
+    <Container>
+        <Text>{children}</Text>
+    </Container>
+);
+
+export default SiteIsNotBeingUsedInfo;
